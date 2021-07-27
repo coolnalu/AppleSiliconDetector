7c0edd3b079eca1b380f775485a0c809fd700ca0
Terry Xu <coolnalu@gmail.com>
2021-07-27 19:04:40 -0400

    Initial Commit

---
+++ Shared/AppleSiliconDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSiliconDetectorApp.swift
+//  Shared
+//
+//  Created by Terry on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleSiliconDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
